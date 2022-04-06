Fix rotation of image

model.py
@@ -47,9 +47,6 @@ class PPNet(nn.Module):
         self.epsilon = 1e-4
         self.void_negative_weight = void_negative_weight
 
-        if void_negative_weight != -0.5:
-            raise NotImplementedError('Not implemented yet')
-
         # prototype_activation_function could be 'log', 'linear',
         # or a generic function that converts distance to similarity score
         self.prototype_activation_function = prototype_activation_function
@@ -277,6 +274,9 @@ class PPNet(nn.Module):
         self.last_layer.weight.data.copy_(
             correct_class_connection * positive_one_weights_locations
             + incorrect_class_connection * negative_one_weights_locations)
+
+        # set weight of prototypes to void class
+        self.last_layer.weight.data[0] = self.void_negative_weight
 
     def _initialize_weights(self):
         for m in self.add_on_layers.modules():

segmentation/data_module.py
@@ -11,9 +11,9 @@ from torch.utils.data import DataLoader
 from segmentation.dataset import SlidingWindowDataset
 from settings import data_path
 
-import torch.multiprocessing
-
-torch.multiprocessing.set_sharing_strategy('file_system')
+# Try this out in case of high RAM usage:
+# import torch.multiprocessing
+# torch.multiprocessing.set_sharing_strategy('file_system')
 
 DataLoader = gin.external_configurable(DataLoader)
 

segmentation/dataset.py
@@ -196,13 +196,13 @@ class SlidingWindowDataset(VisionDataset):
 
             # image has additional margin, so we add it to get the central pixel location in the image
             img = img[
-                window_center2 - margin_size + self.image_margin_size:
-                window_center2 + margin_size + self.image_margin_size,
-                window_center1 - margin_size + self.image_margin_size:
-                window_center1 + margin_size + self.image_margin_size
+              window_center1 - margin_size + self.image_margin_size:
+              window_center1 + margin_size + self.image_margin_size,
+              window_center2 - margin_size + self.image_margin_size:
+              window_center2 + margin_size + self.image_margin_size
             ]
 
-            img = torch.tensor(img).permute(2, 1, 0) / 256
+            img = torch.tensor(img).permute(2, 0, 1) / 256
 
             if self.transform is not None:
                 img = self.transform(img)

segmentation/module.py
@@ -148,9 +148,10 @@ class SlidingWindowModule(LightningModule):
             inverted_distances_to_nontarget_prototypes, _ = \
                 torch.max((max_dist - min_distances) * prototypes_of_wrong_class, dim=1)
 
-            # ignore separation loss for 'void' class
-            # (we do not care if prototypes are "similar" to void class - we do not use prototypes for it)
-            inverted_distances_to_nontarget_prototypes[target == 0] = max_dist
+            # optionally: ignore separation loss for 'void' class
+            # (we might not care if prototypes are "similar" to void class - we do not use prototypes for it)
+            # inverted_distances_to_nontarget_prototypes[target == 0] = max_dist
+
             separation_cost = torch.mean(max_dist - inverted_distances_to_nontarget_prototypes)
 
             # calculate avg cluster cost