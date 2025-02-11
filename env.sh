79832be40d10a97d1baba6e1fb631c5b652fbc99
Tallha <173873873+tijaz17skane@users.noreply.github.com>
2025-02-11 02:20:29 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,14 +2,20 @@
 # run this script before any python script in this repository
 
 # edit these variables if needed
+# SOURCE_DATA_PATH is the datapath to the CityScapes or Pancreas Dataset
 export SOURCE_DATA_PATH='./data'
+# DATA_PATH is the datapath to the CityScapes or Pancreas Dataset, this is more of a repetition. SOURCE_DATA_PATH barely gets used anywhere
 export DATA_PATH='./data'
+# LOG_DIR is for training logs
 export LOG_DIR='./logs'
-
+# DATA_PATH
 export RESULTS_DIR='./results'
 
 
 # set it if using neptune
+# NEPTUNE for monitoring. This was a little tedious to get started with. beware of the latest neptune version. 
+# Im using neptune-client           0.14.3
+
 export USE_NEPTUNE=0  # set to 1 to use neptune
 export NEPTUNE_API_TOKEN=""  # set your neptune api token
 export NEPTUNE_PROJECT=""  # set your neptune project name
